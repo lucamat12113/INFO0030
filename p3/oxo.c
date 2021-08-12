8033731d50d4296b8f136fbf93f390352c339378
lucamat12113 <colosus6015@gmail.com>
2021-08-12 23:05:44 +0200

    t'as capté on voit comment ca marche en fait

--- p3/oxo.c
+++ p3/oxo.c
@@ -11,9 +11,14 @@ int main(int argc, char **argv){
     gtk_init(&argc, &argv);
 
     GtkWidget *window;
-    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    g_signal_connect(G_OBJECT(window), "destroy", G_CALLBACK(detruire), NULL);
-    gtk_widget_show(window);
+    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);//creation fenetre
+
+    g_signal_connect(G_OBJECT(window), "destroy", G_CALLBACK(detruire), NULL);//fermeture fenetre si signal
+
+    void gtk_window_set_title(window, OXO);//titre de la fenetre
+
+
+    gtk_widget_show(window);//affichage fenetre
 
     gtk_main();
 
