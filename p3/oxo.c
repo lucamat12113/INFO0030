131161c10d6cb1db9dbf9071d351b79064363a8c
lucamat12113 <colosus6015@gmail.com>
2021-08-12 23:01:57 +0200

    on apprend petit à petit

--- p3/oxo.c
+++ p3/oxo.c
@@ -2,14 +2,21 @@
 #include <stdio.h>
 #include <gtk/gtk.h>
 
+
+void detruire(GtkWidget *pF, gpointer data){
+    gtk_main_quit();
+}
+
 int main(int argc, char **argv){
     gtk_init(&argc, &argv);
 
     GtkWidget *window;
     window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    g_signal_connect(G_OBJECT(window), "destroy", G_CALLBACK(detruire), NULL);
     gtk_widget_show(window);
 
     gtk_main();
+
 
 return EXIT_SUCCESS;
 }//end main()
