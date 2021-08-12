a079486615983de5c3f6fc534f0556fed45504f7
lucamat12113 <colosus6015@gmail.com>
2021-08-12 22:42:25 +0200

    afficher une fenetre

--- p3/oxo.c
+++ p3/oxo.c
@@ -3,8 +3,13 @@
 #include <gtk/gtk.h>
 
 int main(int argc, char **argv){
-    
-return 0;
+    gtk_init(&argc, &argv);
 
+    GtkWidget *window;
+    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_widget_show(window);
 
+    gtk_main();
+
+return EXIT_SUCCESS;
 }//end main()
