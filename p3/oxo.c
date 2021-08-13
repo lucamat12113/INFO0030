1f99133f637261b02fdd82608191abb52cf650f9
lucamat12113 <colosus6015@gmail.com>
2021-08-13 10:19:00 +0200

    tjrs l'erreur...

--- p3/oxo.c
+++ p3/oxo.c
@@ -1,27 +1,80 @@
 #include <stdlib.h>
-#include <stdio.h>
 #include <gtk/gtk.h>
+#include <gdk/gdkkeysyms.h>
 
+GtkWidget *creer_fenetre(){
+  GtkWidget *pF = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+  gtk_window_set_title(GTK_WINDOW(pF), "Un Menu Complexe");
+  gtk_window_set_default_size(GTK_WINDOW(pF), 320, 200);
 
-void detruire(GtkWidget *pF, gpointer data){
-    gtk_main_quit();
-}
+  g_signal_connect(G_OBJECT(pF), "destroy", G_CALLBACK(gtk_main_quit), NULL);
 
-int main(int argc, char **argv){
-    gtk_init(&argc, &argv);
+  return pF;
+}//fin creer_fenetre()
 
-    GtkWidget *window;
-    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);//creation fenetre
+GtkWidget *creer_menu(GtkWidget *pFenetre){
+  //Déclaration des variables
+  GtkWidget *barre_menu;
+  GtkWidget *menu_fichier;
+  GtkWidget *item_fichier;
+  GtkWidget *item_nouveau;
+  GtkWidget *item_ouvrir;
+  GtkWidget *item_quitter;
+  GtkWidget *item_separateur;
+  GtkAccelGroup *accelerateur = NULL;
 
-    g_signal_connect(G_OBJECT(window), "destroy", G_CALLBACK(detruire), NULL);//fermeture fenetre si signal
+  //barre de menu
+  barre_menu = gtk_menu_bar_new();
 
-    void gtk_window_set_title(window, OXO);//titre de la fenetre
+  //le menu "Fichier"
+  menu_fichier = gtk_menu_new();
 
+  //accélérateur
+  accelerateur = gtk_accel_group_new();
+  gtk_window_add_accel_group(GTK_WINDOW(pFenetre), accelerateur);
 
-    gtk_widget_show(window);//affichage fenetre
+  //création des items avec images
+  item_fichier = gtk_menu_item_new_with_mnemonic("_Fichier");
+  item_nouveau = gtk_image_menu_item_new_from_stock(GTK_STOCK_NEW, NULL);
+  item_ouvrir = gtk_image_menu_item_new_from_stock(GTK_STOCK_OPEN, NULL);
+  item_separateur = gtk_separator_menu_item_new();
+  item_quitter = gtk_image_menu_item_new_from_stock(GTK_STOCK_QUIT, accelerateur);
 
-    gtk_main();
+  gtk_widget_add_accelerator(item_quitter, "activate", accelerateur,
+  GDK_q, GDK_CONTROL_MASK, GTK_ACCEL_VISIBLE);
 
+  //attacher les items
+  gtk_menu_item_set_submenu(GTK_MENU_ITEM(item_fichier), menu_fichier);
+  gtk_menu_shell_append(GTK_MENU_SHELL(menu_fichier), item_nouveau);
+  gtk_menu_shell_append(GTK_MENU_SHELL(menu_fichier), item_ouvrir);
+  gtk_menu_shell_append(GTK_MENU_SHELL(menu_fichier), item_separateur);
+  gtk_menu_shell_append(GTK_MENU_SHELL(menu_fichier), item_quitter);
+  gtk_menu_shell_append(GTK_MENU_SHELL(barre_menu), item_fichier);
 
-return EXIT_SUCCESS;
-}//end main()
+  //les signaux
+  g_signal_connect(G_OBJECT(item_quitter), "activate", G_CALLBACK(gtk_main_quit), NULL);
+
+  return barre_menu;
+}//fin creer_menu()
+
+int main( int argc, char **argv){
+  GtkWidget *pFenetre;
+  GtkWidget *pBarreMenu;
+  GtkWidget *vBox;
+
+  gtk_init(&argc, &argv);
+
+  pFenetre = creer_fenetre();
+  pBarreMenu = creer_menu(pFenetre);
+
+  vBox = gtk_vbox_new(FALSE, 0);
+  gtk_container_add(GTK_CONTAINER(pFenetre), vBox);
+
+  gtk_box_pack_start(GTK_BOX(vBox), pBarreMenu, FALSE, FALSE, 3);
+
+  gtk_widget_show_all(pFenetre);
+
+  gtk_main();
+
+  return EXIT_SUCCESS;
+}//fin main()
